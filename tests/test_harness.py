020719768639572450e9805017ac3dc80035f6e0
Rajaram Padmanathan <rpadmanathan@paypal.com>
2019-02-01 11:25:17 -0800

    Release prep

--- tests/test_harness.py
+++ tests/test_harness.py
@@ -5,7 +5,7 @@ from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
 class TestHarness(unittest.TestCase):
 
     def setUp(self):
-        client_id = "AVNCVvV9oQ7qee5O8OW4LSngEeU1dI7lJAGCk91E_bjrXF2LXB2TK2ICXQuGtpcYSqs4mz1BMNQWuso1"
-        client_secret = "EDQzd81k-1z2thZw6typSPOTEjxC_QbJh6IithFQuXdRFc7BjVht5rQapPiTaFt5RC-HCa1ir6mi-H5l"
+        client_id = "<<PAYPAL-CLIENT-ID>>"
+        client_secret = "<<PAYPAL-CLIENT-SECRET>>"
         self.environment = SandboxEnvironment(client_id=client_id, client_secret=client_secret)
         self.client = PayPalHttpClient(self.environment)
